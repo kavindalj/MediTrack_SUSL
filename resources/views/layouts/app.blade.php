0908717be5f4bc0b1638dc3152cc3ebc58c1a045
ShehaanN <shehannadeesha2580@gmail.com>
2025-09-09 20:30:38 +0530

    feat: add category management with pagination and CRUD modals
    - Implement category listing with pagination
    - Add modal for creating new categories
    - Add modal for editing categories
    - Add modal for deleting categories

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'MediTrack Dashboard')</title>
     
     <!-- Bootstrap CSS (Local) -->
@@ -35,6 +36,10 @@
         </div>
     </div>
 
+    <!-- jQuery  -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+
     <!-- Bootstrap JS (Local) -->
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     
@@ -52,6 +57,7 @@
             }
         });
     </script>
+   
     
     @yield('scripts')
 </body>
